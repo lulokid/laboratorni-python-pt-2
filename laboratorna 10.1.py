ebc163634ba6cc57692c9278d4801dff11c9129f
lulokid <romankondratenko10@gmail.com>
2025-11-23 00:34:04 +0200

    laboratorna 10.1.py

--- laboratorna 10.1.py
+++ laboratorna 10.1.py
@@ -2,11 +2,11 @@
 # Оберіть суцільний тип лінії, задайте колір та товщину графіку та позначте осі, виведіть назву графіка на екран, вставте легенду.
 # Використайте бібліотеку Matplotlib.
 import numpy as np
-import matplotlib.pyplot as plt  # Необхідно імпортувати matplotlib для побудови графіка
+import matplotlib.pyplot as plt 
 
-t = np.linspace(0.01, 10, 101)  # Починаємо з 1, щоб уникнути ділення на нуль
+t = np.linspace(0.01, 10, 101)  # Починаємо з 0.01, щоб уникнути ділення на нуль
 
-y = -5 * np.cos(10 * t) * np.sin(3 * t) / np.sqrt(t)  # Виправлено синтаксис: np.sqrt замість ^(1/2)
+y = -5 * np.cos(10 * t) * np.sin(3 * t) / np.sqrt(t) 
 
 plt.plot(t, y, label='Y(x) = -5*cos(10*x)*sin(3*x)/(x^(1/2))', color="black", linewidth=2)
 
@@ -15,4 +15,5 @@ plt.xlabel('t', fontsize=12, color='blue')  # Вісь абсцис
 plt.ylabel('y', fontsize=12, color='blue')  # Вісь ординат
 plt.legend()
 plt.grid(True)
-plt.show()  # Не забудьте викликати show() для відображення графіка
+
+plt.show()  
